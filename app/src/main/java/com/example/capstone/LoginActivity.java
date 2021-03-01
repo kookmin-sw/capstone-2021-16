b969902ab2f2b0354b13c510b4e118366daac2b1
KimHyeonSeo <50789483+Chord-West@users.noreply.github.com>
2021-03-01 20:26:18 +0900

    update login ui design

--- app/src/main/java/com/example/capstone/LoginActivity.java
+++ app/src/main/java/com/example/capstone/LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.capstone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +12,29 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        findViewById(R.id.login_btn).setOnClickListener(onClickListener);
+        findViewById(R.id.login_signup_btn).setOnClickListener(onClickListener);
     }
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.login_btn:
+                    login();
+                    break;
+                case R.id.login_signup_btn:
+                    Intent intent = new Intent(LoginActivity.this,SignupActivity.class);
+                    startActivity(intent);
+                    break;
+            }
+        }
+    };
+
+    private void login(){
+        Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+        startActivity(intent);
+
+    }
+
 }
