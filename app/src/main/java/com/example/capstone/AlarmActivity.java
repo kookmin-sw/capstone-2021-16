8cf474cd8c10dd0d801ea8670dabfd80f09ade59
hamseokmin <hahmsm@kookmin.ac.kr>
2021-03-12 17:47:02 +0900

    add friendlist to alarm

--- app/src/main/java/com/example/capstone/AlarmActivity.java
+++ app/src/main/java/com/example/capstone/AlarmActivity.java
@@ -1,4 +1,114 @@
 package com.example.capstone;
 
-public class AlarmActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class AlarmActivity extends AppCompatActivity {
+    SingerAdapter adapter;
+
+    EditText editText;
+    EditText editText2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm);
+
+        editText = (EditText) findViewById(R.id.editText);
+        editText2 = (EditText) findViewById(R.id.editText2);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+
+        // 어댑터 안에 데이터 담기
+        adapter = new SingerAdapter();
+
+        adapter.addItem(new SingerItem("이헌수", "010-1111-1111", 1));
+        adapter.addItem(new SingerItem("이주윤", "010-2222-2222", 2));
+        adapter.addItem(new SingerItem("이선용", "010-3333-3333", 3));
+        adapter.addItem(new SingerItem("김현서", "010-4444-4444", 4));
+        adapter.addItem(new SingerItem("함석민", "010-5555-5555", 5));
+        adapter.addItem(new SingerItem("임호딱", "010-6666-6666", 6));
+
+        // 리스트 뷰에 어댑터 설정
+        listView.setAdapter(adapter);
+
+        // 이벤트 처리 리스너 설정
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                SingerItem item = (SingerItem) adapter.getItem(position);
+                Toast.makeText(getApplicationContext(), "선택 :"+item.getName(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        // 버튼 눌렀을 때 우측 이름, 전화번호가 리스트뷰에 포함되도록 처리
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = editText.getText().toString();
+                String mobile = editText2.getText().toString();
+
+                adapter.addItem(new SingerItem(name, mobile, 1));
+                adapter.notifyDataSetChanged();
+            }
+        });
+    }
+
+    class SingerAdapter extends BaseAdapter {
+        ArrayList<SingerItem> items = new ArrayList<SingerItem>();
+
+
+        // Generate > implement methods
+        @Override
+        public int getCount() {
+            return items.size();
+        }
+
+        public void addItem(SingerItem item) {
+            items.add(item);
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return items.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            // 뷰 객체 재사용
+            SingerItemView view = null;
+            if (convertView == null) {
+                view = new SingerItemView(getApplicationContext());
+            } else {
+                view = (SingerItemView) convertView;
+            }
+
+            SingerItem item = items.get(position);
+
+            view.setName(item.getName());
+            view.setMobile(item.getMobile());
+            view.setImage(item.getResId());
+
+
+            return view;
+        }
+    }
 }
