d6e4b681fa719445be91194e7f33215b2e04a1ad
Leejooyoon <ljy9795@naver.com>
2021-03-26 23:23:45 +0900

    master files

--- app/lib/pages/addpromise.dart
+++ app/lib/pages/addpromise.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_database/firebase_database.dart';
-import '../data/memo.dart';
 
 class AddPromise extends StatefulWidget {
-  final DatabaseReference reference;
-  AddPromise(this.reference);
+  AddPromise({Key key}) : super(key: key);
 
   @override
   _AddPromiseState createState() => _AddPromiseState();
@@ -35,63 +32,11 @@ Widget _appbarWidget() {
 }
 
 class _AddPromiseState extends State<AddPromise> {
-  TextEditingController titleController;
-  TextEditingController contentController;
-
-  @override
-  void initState() {
-    super.initState();
-    titleController = TextEditingController();
-    contentController = TextEditingController();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('약속 추가'),
-      ),
-      body: Container(
-        padding: EdgeInsets.all(20),
-        child: Center(
-          child: Column(
-            children: <Widget>[
-              TextField(
-                controller: titleController,
-                decoration: InputDecoration(
-                    labelText: '약속 이름', fillColor: Colors.blueAccent),
-              ),
-              Expanded(
-                  child: TextField(
-                    controller: contentController,
-                    keyboardType: TextInputType.multiline,
-                    maxLines: 100,
-                    decoration: InputDecoration(labelText: '내용'),
-                  )),
-              FlatButton(
-                onPressed: () {
-                  widget.reference
-                      .push()
-                      .set(Memo(
-                      titleController.value.text,
-                      contentController.value.text,
-                      DateTime.now().toIso8601String())
-                      .toJson())
-                      .then((_) {
-                    Navigator.of(context).pop();
-                  });
-                },
-                child: Text('추가하기'),
-                shape:
-                OutlineInputBorder(borderRadius: BorderRadius.circular(1)),
-              )
-            ],
-          ),
-        ),
-      ),
+      appBar: _appbarWidget(),
+      body: Text("약속 추가 페이지"),
     );
-    // return Scaffold(
-    //   body: Text("약속 추가"),
-    // );
   }
 }
