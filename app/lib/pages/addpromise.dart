d02c277bf37fbeab318e945e073861a34345f905
Leejooyoon <ljy9795@naver.com>
2021-03-26 23:32:38 +0900

    파이어베이스 파일 반영

--- app/lib/pages/addpromise.dart
+++ app/lib/pages/addpromise.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
+import '../data/memo.dart';
 
 class AddPromise extends StatefulWidget {
-  AddPromise({Key key}) : super(key: key);
+  final DatabaseReference reference;
+  AddPromise(this.reference);
 
   @override
   _AddPromiseState createState() => _AddPromiseState();
@@ -32,11 +35,63 @@ Widget _appbarWidget() {
 }
 
 class _AddPromiseState extends State<AddPromise> {
+  TextEditingController titleController;
+  TextEditingController contentController;
+
+  @override
+  void initState() {
+    super.initState();
+    titleController = TextEditingController();
+    contentController = TextEditingController();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: _appbarWidget(),
-      body: Text("약속 추가 페이지"),
+      appBar: AppBar(
+        title: Text('약속 추가'),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              TextField(
+                controller: titleController,
+                decoration: InputDecoration(
+                    labelText: '약속 이름', fillColor: Colors.blueAccent),
+              ),
+              Expanded(
+                  child: TextField(
+                    controller: contentController,
+                    keyboardType: TextInputType.multiline,
+                    maxLines: 100,
+                    decoration: InputDecoration(labelText: '내용'),
+                  )),
+              FlatButton(
+                onPressed: () {
+                  widget.reference
+                      .push()
+                      .set(Memo(
+                      titleController.value.text,
+                      contentController.value.text,
+                      DateTime.now().toIso8601String())
+                      .toJson())
+                      .then((_) {
+                    Navigator.of(context).pop();
+                  });
+                },
+                child: Text('추가하기'),
+                shape:
+                OutlineInputBorder(borderRadius: BorderRadius.circular(1)),
+              )
+            ],
+          ),
+        ),
+      ),
     );
+    // return Scaffold(
+    //   body: Text("약속 추가"),
+    // );
   }
 }
