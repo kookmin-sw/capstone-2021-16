5caa58e24301a737b6aae2448db16e5bc82c79cf
Hun-Su <50570659+Hun-Su@users.noreply.github.com>
2021-05-14 15:15:44 +0900

    Update Category.dart
    Category button update

--- app/lib/pages/Category.dart
+++ app/lib/pages/Category.dart
@@ -1,455 +1,135 @@
 import 'package:flutter/material.dart';
-import 'message.dart';
-import 'notification.dart';
-import 'calendar.dart';
 
-class AddPromise extends StatefulWidget {
-  AddPromise({Key key}) : super(key: key);
+class Agendas {
+  final List date;
+  final String title;
+  final List people;
 
+  Agendas(this.date, this.title, this.people);
+}
+class AgendasList extends StatefulWidget {
   @override
-  _AddPromiseState createState() => _AddPromiseState();
+  _AgendasListState createState() => _AgendasListState();
 }
 
-class _AddPromiseState extends State<AddPromise> {
-  Widget _appbarWidget() {
-    return AppBar(
-      // AppBar
-      elevation: 0,
-      title: GestureDetector(
-        onTap: () {
-          // 클릭했을 때 Callback이 이 쪽으로 옴
-          print("click");
-        },
-        child: Row(
-          children: [
-            SizedBox(width: 5), //Padding이랑 같은 효과
-            Text("약속 추가"),
-          ],
-        ),
-      ),
-      actions: [
-        IconButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => MessagesList()), // Move to Message
-              );
-            }, icon: Image.asset('assets/images/message.png')),
-        IconButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => NotesList()), // Move to Notice
-              );
-            }, icon: Image.asset('assets/images/bell.png'))
-      ], // 가운데 이름
-    );
-  }
+class _AgendasListState extends State<AgendasList> {
+  static List<Agendas> notes = [
+    Agendas([4, 28], "운동", [3, 5]),
+    Agendas([4, 29], "이벤트", [1, 3]),
+    Agendas([4, 30], "소모임", [1, 3]),
+  ];
 
-  final _controller = TextEditingController();
-  String promise = '';
-  bool repeat = false;
-  bool place = false;
-  final _month = [
-    '1월',
-    '2월',
-    '3월',
-    '4월',
-    '5월',
-    '6월',
-    '7월',
-    '8월',
-    '9월',
-    '10월',
-    '11월',
-    '12월'
-  ];
-  final _date1 = [
-    '1일',
-    '2일',
-    '3일',
-    '4일',
-    '5일',
-    '6일',
-    '7일',
-    '8일',
-    '9일',
-    '10일',
-    '11일',
-    '12일',
-    '13일',
-    '14일',
-    '15일',
-    '16일',
-    '17일',
-    '18일',
-    '19일',
-    '20일',
-    '21일',
-    '22일',
-    '23일',
-    '24일',
-    '25일',
-    '26일',
-    '27일',
-    '28일',
-    '29일',
-    '30일',
-    '31일'
-  ];
-  final _date2 = [
-    '1일',
-    '2일',
-    '3일',
-    '4일',
-    '5일',
-    '6일',
-    '7일',
-    '8일',
-    '9일',
-    '10일',
-    '11일',
-    '12일',
-    '13일',
-    '14일',
-    '15일',
-    '16일',
-    '17일',
-    '18일',
-    '19일',
-    '20일',
-    '21일',
-    '22일',
-    '23일',
-    '24일',
-    '25일',
-    '26일',
-    '27일',
-    '28일',
-    '29일',
-    '30일'
-  ];
-  final _date3 = [
-    '1일',
-    '2일',
-    '3일',
-    '4일',
-    '5일',
-    '6일',
-    '7일',
-    '8일',
-    '9일',
-    '10일',
-    '11일',
-    '12일',
-    '13일',
-    '14일',
-    '15일',
-    '16일',
-    '17일',
-    '18일',
-    '19일',
-    '20일',
-    '21일',
-    '22일',
-    '23일',
-    '24일',
-    '25일',
-    '26일',
-    '27일',
-    '28일',
-    '29일'
-  ];
-  final _time = [
-    '00시',
-    '01시',
-    '02시',
-    '03시',
-    '04시',
-    '05시',
-    '06시',
-    '07시',
-    '08시',
-    '09시',
-    '10시',
-    '11시',
-    '12시',
-    '13시',
-    '14시',
-    '15시',
-    '16시',
-    '17시',
-    '18시',
-    '19시',
-    '20시',
-    '21시',
-    '22시',
-    '23시'
-  ];
-  final _minute = [
-    '00분',
-    '05분',
-    '10분',
-    '15분',
-    '20분',
-    '25분',
-    '30분',
-    '35분',
-    '40분',
-    '45분',
-    '50분',
-    '55분'
-  ];
-  var selectmonth = '1월';
-  var selectdate = '1일';
-  var starttime = '00시';
-  var startminute = "00분";
-  var endtime = "00시";
-  var endminute = "00분";
+  final notes_temp = new List<Agendas>.from(notes);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: _appbarWidget(),
-        body: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Container(
-          // 약속 입력
-          margin: EdgeInsets.only(top: 20, left: 30, right: 30),
-          height: 150,
-          child: TextField(
-            controller: _controller,
-            decoration: InputDecoration(
-              enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.black)),
-              hintText: '약속 내용을 입력하세요.',
-            ),
-            style: TextStyle(fontSize: 20),
-            maxLines: 5,
-            onChanged: (Text) {
-              promise = Text; // 현재 Textfield의 내용을 저장
-              //print("$Text"); // 확인
-            },
-          ),
-        ),
-        Container(
-            // 반복 여부 설정
-            color: Colors.white60,
-            margin: EdgeInsets.only(top: 20, left: 30, right: 30),
-            child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Container(
-                      alignment: Alignment.centerLeft,
-                      child: TextButton(
-                          child: Text("반복"),
-                          style: TextButton.styleFrom(
-                            textStyle: TextStyle(fontSize: 20),
-                          ))),
-                  Container(
-                      // 장소 여부 설정
-                      margin: EdgeInsets.only(left: 220),
-                      alignment: Alignment.centerRight,
-                      child: Switch(
-                        value: repeat,
-                        onChanged: (value) {
-                          setState(() {
-                            repeat = value;
-                          });
-                        },
-                        activeTrackColor: Colors.green,
-                        activeColor: Colors.white10,
-                      ))
-                ])),
-        Container(
-            // 날짜 설정
-            color: Colors.white60,
-            margin: EdgeInsets.only(top: 20, left: 30, right: 30),
-            child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Container(
-                      alignment: Alignment.centerLeft,
-                      child: TextButton(
-                          child: Text("장소"),
-                          style: TextButton.styleFrom(
-                            textStyle: TextStyle(fontSize: 20),
-                          ))),
-                  Container(
-                      margin: EdgeInsets.only(left: 220),
-                      alignment: Alignment.centerRight,
-                      child: Switch(
-                        value: place,
-                        onChanged: (value) {
-                          setState(() {
-                            place = value;
-                          });
-                        },
-                        activeTrackColor: Colors.green,
-                        activeColor: Colors.white10,
-                      ))
-                ])),
-        Container(
-            margin: EdgeInsets.only(top: 20, left: 30, right: 30),
-            color: Colors.white60,
-            child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Container(
-                      alignment: Alignment.centerLeft,
-                      child: TextButton(
-                          child: Text("날짜"),
-                          style: TextButton.styleFrom(
-                            textStyle: TextStyle(fontSize: 20),
-                          ))),
-                  Container(
-                      margin: EdgeInsets.only(left: 120),
-                      child: DropdownButton(
-                        value: selectmonth,
-                        items: _month.map(
-                          (value) {
-                            return DropdownMenuItem(
-                              value: value,
-                              child: Text(value),
-                            );
-                          },
-                        ).toList(),
-                        onChanged: (value) {
-                          setState(() {
-                            selectmonth = value;
-                          });
-                        },
-                      )),
-                  Container(
-                      margin: EdgeInsets.only(left: 20),
-                      child: DropdownButton(
-                        value: selectdate,
-                        items: _date1.map(
-                          (value) {
-                            return DropdownMenuItem(
-                              value: value,
-                              child: Text(value),
-                            );
-                          },
-                        ).toList(),
-                        onChanged: (value) {
-                          setState(() {
-                            selectdate = value;
-                          });
-                        },
-                      )),
-                ])),
-        Container(
-            // 시간설정
-            margin: EdgeInsets.only(top: 20, left: 30, right: 30),
-            color: Colors.white60,
-            child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Container(
-                      alignment: Alignment.centerLeft,
-                      child: TextButton(
-                          child: Text("시간"),
-                          style: TextButton.styleFrom(
-                            textStyle: TextStyle(fontSize: 20),
-                          ))),
-                  Container(
-                      margin: EdgeInsets.only(left: 10),
-                      child: DropdownButton(
-                        value: starttime,
-                        items: _time.map(
-                          (value) {
-                            return DropdownMenuItem(
-                              value: value,
-                              child: Text(value),
-                            );
-                          },
-                        ).toList(),
-                        onChanged: (value) {
-                          setState(() {
-                            starttime = value;
-                          });
-                        },
-                      )),
-                  Container(
-                      margin: EdgeInsets.only(left: 5),
-                      child: DropdownButton(
-                        value: startminute,
-                        items: _minute.map(
-                          (value) {
-                            return DropdownMenuItem(
-                              value: value,
-                              child: Text(value),
-                            );
-                          },
-                        ).toList(),
-                        onChanged: (value) {
-                          setState(() {
-                            startminute = value;
-                          });
-                        },
-                      )),
-                  Container(
-                      width: 15,
-                      child: TextButton(
-                          child: Text("~"),
-                          style: TextButton.styleFrom(
-                            textStyle: TextStyle(fontSize: 15),
-                          ))),
-                  Container(
-                      margin: EdgeInsets.only(left: 10),
-                      child: DropdownButton(
-                        value: endtime,
-                        items: _time.map(
-                          (value) {
-                            return DropdownMenuItem(
-                              value: value,
-                              child: Text(value),
-                            );
-                          },
-                        ).toList(),
-                        onChanged: (value) {
-                          setState(() {
-                            endtime = value;
-                          });
-                        },
-                      )),
-                  Container(
-                      margin: EdgeInsets.only(left: 5),
-                      child: DropdownButton(
-                        value: endminute,
-                        items: _minute.map(
-                          (value) {
-                            return DropdownMenuItem(
-                              value: value,
-                              child: Text(value),
-                            );
-                          },
-                        ).toList(),
-                        onChanged: (value) {
-                          setState(() {
-                            endminute = value;
-                          });
-                        },
-                      )),
-                ])),
-        Container(
-          //추가하기 버튼
-            margin: EdgeInsets.only(top: 30, right: 30, left: 240),
-            alignment: Alignment.centerRight,
-            color: Colors.grey,
-            child: TextButton(
-                child: Text("약속 추가하기"),
-                onPressed: () {setState(() {
-                  appointments.add(Appointment(
-
-                  ))
-                });,
-                style: TextButton.styleFrom(
-                  textStyle: TextStyle(fontSize: 20),
-                )))
-      ],
-        ));
+      drawer: Drawer(
+          child: ListView(
+              padding: EdgeInsets.zero,
+              children: <Widget> [
+                DrawerHeader(
+                  decoration: BoxDecoration(
+                    color: Color(0xff18A0FB),
+                  ),
+                  child: Text(
+                      '카테고리',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24
+                      )
+                  ),
+                ),
+                ListTile(
+                    title: Text('운동'),
+                    onTap: () { setState(() {
+                      notes_temp.clear();
+                      notes_temp.insertAll(0, notes);
+                      notes_temp.removeWhere((item) => item.title != '운동');
+                    });
+                    }
+                ),
+                ListTile(
+                    title: Text('이벤트'),
+                    onTap: () { setState(() {
+                      notes_temp.clear();
+                      notes_temp.insertAll(0, notes);
+                      notes_temp.removeWhere((item) => item.title != '이벤트');
+                    });
+                    }
+                ),
+                ListTile(
+                    title: Text('소모임'),
+                    onTap: () {
+                      setState(() {
+                        notes_temp.clear();
+                        notes_temp.insertAll(0, notes);
+                        notes_temp.removeWhere((item) => item.title != '소모임');
+                      });
+                    }
+                ),
+                ListTile(
+                    title: Text('모두'),
+                    onTap: () {
+                      setState(() {
+                        notes_temp.clear();
+                        notes_temp.insertAll(0, notes);
+                      });
+                    }
+                ),
+              ]
+          )
+      ),
+      backgroundColor: Colors.white,
+      body: ListView.builder(
+          itemCount: notes_temp.length,
+          itemBuilder: (context, index) {
+            return Card(
+              margin: EdgeInsets.fromLTRB(5, 5, 5, 5),
+              child: ListTile(
+                onTap: () {},
+                title: Text(
+                    notes_temp[index].title,
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Color(0xff18A0FB),
+                      fontWeight: FontWeight.bold,
+                    )
+                ),
+                leading: Text.rich(
+                  TextSpan(
+                      text: notes_temp[index].date[0].toString(),
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Color(0xff18A0FB),
+                      ),
+                      children: <TextSpan> [
+                        TextSpan(text: '/'),
+                        TextSpan(text: notes_temp[index].date[1].toString())
+                      ]
+                  ),
+                ),
+                trailing: Text.rich(
+                  TextSpan(
+                      text: notes_temp[index].people[0].toString(),
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.black,
+                      ),
+                      children: <TextSpan> [
+                        TextSpan(text: '/'),
+                        TextSpan(text: notes_temp[index].people[1].toString())
+                      ]
+                  ),
+                ),
+              ),
+            );
+          }
+      ),
+    );
   }
 }
 
+void main() {
+  runApp(MaterialApp(home: AgendasList()));
+}
